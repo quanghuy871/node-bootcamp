698a11109d0a04824b74200ca8955015d31c2e05
Huy Nguyen <quanghuy871@gmail.com>
2022-01-11 23:07:11 +0700

    Add All

--- 4-natours/routes/reviewRoutes.js
+++ 4-natours/routes/reviewRoutes.js
@@ -6,3 +6,4 @@ const authControllers = require('./../controllers/authController');
 router.route('/').get(reviewControllers.getAllReviews).post(reviewControllers.createReview);
 
 module.exports = router;
+
